b7e1235c2594cbeb2c9e60cac2deb095446a84b7
Brad Dickinson <brad@blabley.org>
2020-06-15 22:03:47 +0200

    Update autounattend.ps1

--- autounattend.ps1
+++ autounattend.ps1
@@ -1,5 +1,5 @@
 #Windows 7+ Auto Install file from blabley.org
 Invoke-WebRequest -UseBasicParsing "https://github.com/blabley/AutoInstall/archive/master.zip" -OutFile "$env:TEMP\AutoInstall.zip" -PassThru
 Expand-Archive -Path "$env:TEMP\AutoInstall.zip" -DestinationPath "$env:TEMP\AutoInstall" -Force
-Move-Item -LiteralPath "$env:TEMP\AutoInstall\AutoInstall-master" -Destination "$env:SystemDrive\Install99" -Force
+Move-Item -LiteralPath "$env:TEMP\AutoInstall\AutoInstall-master" -Destination "$env:SystemDrive\Install" -Force
 cmd /c "%systemdrive%\Install\AutoInstall.cmd"
